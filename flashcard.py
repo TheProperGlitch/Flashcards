ed1a3004d0acf359acfcaea5e10128ccfc79df44
Bowie Vance <bvancepantz@gmail.com>
2023-03-14 09:44:05 -0700

    starting up

---
+++ flashcard.py
@@ -0,0 +1,5 @@
+flashcards = [
+  ["What was the first ever video game called: (1: Pong, 2: Space Invaders, 3: BitCrunch, 4: Tennis for Two)", 4],
+  ["The number one most sold game is Minecraft. What is number 2? (1: Tetris, 2:GTA5, 3:Red Dead Redemption 4:Legend of Zelda; Breath of the Wild)", 2],
+  ["Which of the following is the highest grossing game of all time? (1: Pac-Man, 2: League of Legends, 3: Dungeon Fighter Online, 4: Minecraft)", 3],
+]
