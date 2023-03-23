d53053608151b8bb3f498706e24766195c6d1cba
Bowie Vance <bvancepantz@gmail.com>
2023-03-23 11:31:17 -0700

    it begins

--- flashcard.py
+++ flashcard.py
@@ -14,8 +14,8 @@ else:
     with open(name+".py","r") as file:
          old_score = file.read()
 flashcards = [
-  ["What was the first ever video game called? \n(1: Pong, 2: Space Invaders, 3: BitCrunch, 4: Tennis for Two)", 4],
-  ["The number one most sold game is Minecraft. What is number 2? \n(1: Tetris, 2:GTA5, 3:Red Dead Redemption 4:Legend of Zelda; Breath of the Wild)", 2],
+  ["What is the starting are called in Hollow Knight? \n (1: King's Pass, 2: Dirtmouth, 3: Forgotten Crossroads, 4: Kingdom's Edge)", 1],
+  ["Which of the three dreamers does Quirrel work for? \n(1: Monomon the Teacher 2: Lurien the Watcher, 3: Herrah the Beast 4: Dreaming Jerry)", 2],
   ["Which of the following is the highest grossing game of all time? \n(1: Pac-Man, 2: League of Legends, 3: Dungeon Fighter Online, 4: Minecraft)", 3],
   ["What was the first video game console played in space? \n(1:Nentendo Gameboy, 2: Sega Saturn, 3: Sony PlayStation, 4: Atari 2600)", 1]
 ]
